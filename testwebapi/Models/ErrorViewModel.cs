b6077dfb32b79ad3f7378e670f3297534884a08c
Roman <roman.makarevych@gmail.com>
2018-06-08 10:41:57 +0300

    init

---
+++ testwebapi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testwebapi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

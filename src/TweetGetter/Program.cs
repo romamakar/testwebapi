6db4be6efa8dffa2acf8bf738181331a20e9def1
Roman <roman.makarevych@gmail.com>
2018-06-12 01:22:51 +0300

    test

--- src/TweetGetter/Program.cs
+++ src/TweetGetter/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace TweetGetter
 {
@@ -8,7 +10,28 @@ namespace TweetGetter
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+           // BuildWebHost(args).Run();
+
+            var config = new ConfigurationBuilder()
+                        .AddCommandLine(args)
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+                        .Build();
+
+            var host = new WebHostBuilder()
+                .UseConfiguration(config)
+                .UseStartup<Startup>()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                  .UseKestrel()
+                  .UseUrls("http://localhost:5000/")
+                  .ConfigureLogging((hostingContext, logging) =>
+                  {
+                      logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                      logging.AddConsole();
+                      logging.AddDebug();
+                  })
+                  .Build();
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
